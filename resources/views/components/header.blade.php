9277088f6323b9645832b3b36a5325089af10cf0
Emanuela91 <emanuela.marson91@gmail.com>
2023-02-13 16:32:40 +0100

    collegato html

---
+++ resources/views/components/header.blade.php
@@ -0,0 +1,1 @@
+<h1>Relazione Utenti</h1>
---
+++ resources/views/pages/home.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.main-layout')
+
+@section('content')
+    <h1>Utenti</h1>
+    
+    {{-- <ul>
+        @foreach ($projects as $project)
+            <li>
+                <a href="{{ route('project.show', $project) }}">
+                    {{ $project -> name }}
+                </a>
+                <br>--------------------- <br>
+            </li>
+        @endforeach
+    </ul> --}}
+@endsection
