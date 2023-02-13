9d34adfc790faa9a5a5a7d990eb72e19d00999f2
Emanuela91 <emanuela.marson91@gmail.com>
2023-02-13 19:40:06 +0100

    fatta factory di person detail e di post e collegato tutto nel database seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,7 +15,9 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call([
-            PersonSeeder::class
+            PersonSeeder::class,
+            PersonDetailSeeder::class,
+            PostSeeder::class,
         ]);
     }
 }
